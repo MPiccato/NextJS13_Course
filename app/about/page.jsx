96d7571dd3d9f69f0321027cdfce5986f1e767f0
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2023-08-21 15:18:18 -0300

    agrego estilos

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,3 +1,9 @@
+
+export const metadata = {
+    title: "Acerca de nosotros - MPiccato"
+}
+
+
 function AboutPage() {
     return <h1>About Martin</h1>
 };
--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,25 @@
 import NavBar from '../components/Navbar'
 
+import { Roboto } from 'next/font/google';
+
+
 export const metadata = {
   title: 'Mi tienda',
   description: 'Aprendiendo NetxJs',
 }
 
+//estilos con NextJS
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  styles: ["italic", "normal"],
+  subsets: ["latin"]
+
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <NavBar />
         {children}
       </body>
